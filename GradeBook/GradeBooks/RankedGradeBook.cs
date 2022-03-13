076a2178e5c13cf4b6f0cdad190bb735b760691e
Kamil <kamil.skalny@microsoft.wsei.edu.pl>
2022-03-13 12:06:44 +0100

    Override 's  method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -29,5 +29,13 @@ namespace GradeBook.GradeBooks{
             else
                 return 'F';
         }
+        public override void CalculateStatistics()
+        {
+            if (this.Students.Count < 5){
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
     }
 }
