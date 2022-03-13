14d696787a31bedaad1ee448ee3d6c009478a73a
Kamil <kamil.skalny@microsoft.wsei.edu.pl>
2022-03-13 11:32:14 +0100

    Creating the `RankedGradeBook` class

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,9 @@
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks{
+    public class RankedGradeBook:BaseGradeBook{
+        public RankedGradeBook(string name):base(name){
+            this.Type = GradeBookType.Ranked;
+        }
+    }
+}
